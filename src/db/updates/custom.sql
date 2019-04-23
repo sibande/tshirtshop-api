e06a10fc7b5cbc6e2278ebc858c5e58afbc9fe04
Jose Sibande <me@sibande.com>
2019-04-23 20:34:54 +0200

    Update more procedures

--- src/db/updates/custom.sql
+++ src/db/updates/custom.sql
@@ -127,6 +127,90 @@ BEGIN
   END IF;
 END$$
 
+-- Create catalog_count_products_on_catalog stored procedure
+DROP PROCEDURE IF EXISTS catalog_count_products_on_catalog $$
+
+CREATE PROCEDURE catalog_count_products_on_catalog()
+BEGIN
+  SELECT COUNT(*) AS products_on_catalog_count
+  FROM   product;
+END$$
+
+-- Create catalog_get_products_on_catalog stored procedure
+DROP PROCEDURE IF EXISTS catalog_get_products_on_catalog $$
+
+CREATE PROCEDURE catalog_get_products_on_catalog(
+  IN inShortProductDescriptionLength INT,
+  IN inProductsPerPage INT, IN inStartItem INT)
+BEGIN
+  PREPARE statement FROM
+    "SELECT   product_id, name,
+              IF(LENGTH(description) <= ?,
+                 description,
+                 CONCAT(LEFT(description, ?),
+                        '...')) AS description,
+              price, discounted_price, thumbnail
+     FROM     product
+     ORDER BY display DESC
+     LIMIT    ?, ?";
+
+  SET @p1 = inShortProductDescriptionLength;
+  SET @p2 = inShortProductDescriptionLength;
+  SET @p3 = inStartItem;
+  SET @p4 = inProductsPerPage;
+
+  EXECUTE statement USING @p1, @p2, @p3, @p4;
+END$$
+
+-- Create catalog_count_products_on_department stored procedure
+DROP PROCEDURE IF EXISTS catalog_count_products_on_department $$
+
+CREATE PROCEDURE catalog_count_products_on_department(IN inDepartmentId INT)
+BEGIN
+  SELECT DISTINCT COUNT(*) AS products_on_department_count
+  FROM            product p
+  INNER JOIN      product_category pc
+                    ON p.product_id = pc.product_id
+  INNER JOIN      category c
+                    ON pc.category_id = c.category_id
+  WHERE           (p.display = 2 OR p.display = 3)
+                  AND c.department_id = inDepartmentId;
+END$$
+
+-- Create catalog_get_products_on_department stored procedure
+DROP PROCEDURE IF EXISTS catalog_get_products_on_department $$
+
+CREATE PROCEDURE catalog_get_products_on_department(
+  IN inDepartmentId INT, IN inShortProductDescriptionLength INT,
+  IN inProductsPerPage INT, IN inStartItem INT)
+BEGIN
+  PREPARE statement FROM
+    "SELECT DISTINCT p.product_id, p.name,
+                     IF(LENGTH(p.description) <= ?,
+                        p.description,
+                        CONCAT(LEFT(p.description, ?),
+                               '...')) AS description,
+                     p.price, p.discounted_price, p.thumbnail
+     FROM            product p
+     INNER JOIN      product_category pc
+                       ON p.product_id = pc.product_id
+     INNER JOIN      category c
+                       ON pc.category_id = c.category_id
+     WHERE           (p.display = 2 OR p.display = 3)
+                     AND c.department_id = ?
+     ORDER BY        p.display DESC
+     LIMIT           ?, ?";
+
+  SET @p1 = inShortProductDescriptionLength;
+  SET @p2 = inShortProductDescriptionLength;
+  SET @p3 = inDepartmentId;
+  SET @p4 = inStartItem;
+  SET @p5 = inProductsPerPage;
+
+  EXECUTE statement USING @p1, @p2, @p3, @p4, @p5;
+END$$
 
 -- Change back DELIMITER to ;
 DELIMITER ;
+
+

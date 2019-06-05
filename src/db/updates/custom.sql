75b6861d1e6c4ffd45197c41ed5496416078ccf0
Jose Sibande <me@sibande.com>
2019-06-05 23:15:23 +0200

    Add missing columns

--- src/db/updates/custom.sql
+++ src/db/updates/custom.sql
@@ -219,12 +219,27 @@ BEGIN
   SELECT     sc.item_id, p.name, sc.attributes,
              COALESCE(NULLIF(p.discounted_price, 0), p.price) AS price,
              sc.quantity, sc.product_id,
-             COALESCE(NULLIF(p.discounted_price, 0), 
+	     p.thumbnail AS image,	
+             COALESCE(NULLIF(p.discounted_price, 0),
                       p.price) * sc.quantity AS subtotal
   FROM       shopping_cart sc
   INNER JOIN product p
                ON sc.product_id = p.product_id
   WHERE      sc.cart_id = inCartId AND sc.buy_now ORDER BY sc.item_id ASC;
+END$$
+
+
+-- Create shopping_cart_get_saved_products stored procedure
+DROP PROCEDURE IF EXISTS shopping_cart_get_saved_products $$
+
+CREATE PROCEDURE shopping_cart_get_saved_products(IN inCartId CHAR(32))
+BEGIN
+  SELECT     sc.item_id, p.name, sc.attributes, p.thumbnail as image,
+             COALESCE(NULLIF(p.discounted_price, 0), p.price) AS price
+  FROM       shopping_cart sc
+  INNER JOIN product p
+               ON sc.product_id = p.product_id
+  WHERE      sc.cart_id = inCartId AND NOT sc.buy_now;
 END$$
 
 

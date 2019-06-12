6fc319491f45c98acdb174e21e97bf5796087bc2
Jose Sibande <me@sibande.com>
2019-06-12 14:02:27 +0200

    Add orders and stripe api documentation

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -8,16 +8,299 @@ var authMiddleware = require('../routes/middlewares/auth');
 
 var orderController = require('../controllers/orderController');
 
-/* Create a Order */
+
+/**
+ * @swagger
+ * tags:
+ *   - name: orders
+ *     description: Everything about orders
+ */
+
+
+/**
+ * Create a Order
+ *
+ *
+ * @swagger
+ * paths:
+ *   /orders:
+ *     post:
+ *       summary: Create a Order
+ *       description: Create a Order
+ *       tags: [orders]
+ *       produces:
+ *         - application/json
+ *       security:
+ *         - ApiKeyAuth: []
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/x-www-form-urlencoded:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 cart_id:
+ *                   description: Cart ID
+ *                   type: string
+ *                 shipping_id:
+ *                   description: Shipping ID
+ *                   type: integer
+ *                 tax_id:
+ *                   description: Tax ID
+ *                   type: string
+ *               required:
+ *                 - cart_id
+ *                 - shipping_id
+ *                 - tax_id
+ *       responses:
+ *         200:
+ *           description: Return the Order ID
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 properties:
+ *                   orderId:
+ *                     type: integer
+ *                     example: 1
+ *         400:
+ *           description: Returns an error object
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Error'
+ *           examples:
+ *             User Key empty:
+ *               status: 401
+ *               code: AUT_01
+ *               message: Authorization code is empty.
+ *             User Key invalid:
+ *               status: 401
+ *               code: AUT_02
+ *               message: Access Unauthorized.
+ *             Empty cart id:
+ *               code: USR_02
+ *               message: Cart id can't be blank
+ *               field: cart_id
+ *               status: 400
+ *             Empty shipping id:
+ *               code: USR_02
+ *               message: Shipping id can't be blank
+ *               field: shipping_id
+ *               status: 400
+ *             Invalid shipping id:
+ *               code: USR_03
+ *               message: Shipping id is not a number
+ *               field: shipping_id
+ *               status: 400
+ *             Empty tax id:
+ *               code: USR_02
+ *               message: Tax id can't be blank
+ *               field: tax_id
+ *               status: 400
+ *             Invalid tax id:
+ *               code: USR_03
+ *               message: Tax id is not a number
+ *               field: tax_id
+ *               status: 400
+ */
 router.post('/', [upload.none(), authMiddleware.verifyToken], orderController.createOrder);
 
 /* Get Info about Order */
-router.get('/:orderId([0-9]+)', [authMiddleware.verifyToken], orderController.getOrderInfo);
 
-/* Get orders by Customer */
+/**
+ * Get Info about Order
+ *
+ *
+ * @swagger
+ * paths:
+ *   /orders/{order_id}:
+ *     get:
+ *       summary: Get Info about Order
+ *       description: Get Info about Order
+ *       tags: [orders]
+ *       produces:
+ *         - application/json
+ *       security:
+ *         - ApiKeyAuth: []
+ *       parameters:
+ *         - in: path
+ *           name: order_id
+ *           required: true
+ *           schema:
+ *             type: integer
+ *       responses:
+ *         200:
+ *           description: Return a object of Order.
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 properties:
+ *                   order_id:
+ *                     type: integer
+ *                     example: 1
+ *                   total_amount:
+ *                     type: string
+ *                     example: "14.95"
+ *                   created_on:
+ *                     type: string
+ *                     example: "2019-06-12T10:24:31.000Z"
+ *                   shipped_on:
+ *                     type: string
+ *                     example: "2019-06-12T10:24:31.000Z"
+ *                   status:
+ *                     type: integer
+ *                     example: 1
+ *                   comments:
+ *                     type: string
+ *                     example: "Order # 18"
+ *                   customer_id:
+ *                     type: integer
+ *                     example: 1
+ *                   auth_code:
+ *                     type: string
+ *                     example: tok_1EkUYxE8uV3JQbVkVh1WLWNg"
+ *                   reference:
+ *                     type: string
+ *                     example: "ch_1EkUYzE8uV3JQbVkbkZkkglW"
+ *                   shipping_id:
+ *                     type: integer
+ *                     example: 4
+ *                   shipping_type:
+ *                     type: string
+ *                     example: "By air (7 days, $25)"
+ *                   shipping_cost:
+ *                     type: string
+ *                     example: 25
+ *                   tax_id:
+ *                     type: integer
+ *                     example: 2
+ *                   tax_type:
+ *                     type: string
+ *                     example: "No Tax"
+ *                   tax_percentage:
+ *                     type: integer
+ *                     example: 0
+ *         400:
+ *           description: Returns an error object
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Error'
+ *           examples:
+ *             User Key empty:
+ *               status: 401
+ *               code: AUT_01
+ *               message: Authorization code is empty.
+ *             User Key invalid:
+ *               status: 401
+ *               code: AUT_02
+ *               message: Access Unauthorized.
+ */
+router.get('/:order_id([0-9]+)', [authMiddleware.verifyToken], orderController.getOrderInfo);
+
+
+/**
+ * Get orders by Customer
+ *
+ *
+ * @swagger
+ * paths:
+ *   /orders/inCustomer:
+ *     get:
+ *       summary: Get orders by Customer
+ *       description: Get orders by Customer
+ *       tags: [orders]
+ *       produces:
+ *         - application/json
+ *       security:
+ *         - ApiKeyAuth: []
+ *       responses:
+ *         200:
+ *           description: Return a array of Orders
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Customer'
+ *         400:
+ *           description: Returns an error object
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Error'
+ *           examples:
+ *             User Key empty:
+ *               status: 401
+ *               code: AUT_01
+ *               message: Authorization code is empty.
+ *             User Key invalid:
+ *               status: 401
+ *               code: AUT_02
+ *               message: Access Unauthorized.
+ */
 router.get('/inCustomer', [authMiddleware.verifyToken], orderController.getCustomerOrders);
 
-/* Get Info about Order */
+/**
+ * Get Info about Order
+ *
+ *
+ * @swagger
+ * paths:
+ *   /orders/shortDetail/{order_id}:
+ *     get:
+ *       summary: Get Info about Order
+ *       description: Get Info about Order
+ *       tags: [orders]
+ *       produces:
+ *         - application/json
+ *       security:
+ *         - ApiKeyAuth: []
+ *       parameters:
+ *         - in: path
+ *           name: order_id
+ *           required: true
+ *           schema:
+ *             type: integer
+ *       responses:
+ *         200:
+ *           description: Return a object of Order.
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 properties:
+ *                   order_id:
+ *                     type: integer
+ *                     example: 1
+ *                   total_amount:
+ *                     type: string
+ *                     example: "14.95"
+ *                   created_on:
+ *                     type: string
+ *                     example: "2019-06-12T10:24:31.000Z"
+ *                   shipped_on:
+ *                     type: string
+ *                     example: "2019-06-12T10:24:31.000Z"
+ *                   status:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: "Test"
+ *         400:
+ *           description: Returns an error object
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Error'
+ *           examples:
+ *             User Key empty:
+ *               status: 401
+ *               code: AUT_01
+ *               message: Authorization code is empty.
+ *             User Key invalid:
+ *               status: 401
+ *               code: AUT_02
+ *               message: Access Unauthorized.
+ */
 router.get('/shortDetail/:orderId([0-9]+)', [authMiddleware.verifyToken], orderController.getOrderDetails);
 
 
--- src/routes/stripe.js
+++ src/routes/stripe.js
@@ -9,10 +9,131 @@ var authMiddleware = require('../routes/middlewares/auth');
 var stripeController = require('../controllers/stripeController');
 
 
-/* This method receive a front-end payment and create a chage. */
+/**
+ * @swagger
+ * tags:
+ *   - name: stripe
+ *     description: Everything about stripe
+ */
+
+/**
+ * This method receive a front-end payment and create a chage
+ *
+ *
+ * @swagger
+ * paths:
+ *   /stripe/charge:
+ *     post:
+ *       summary: This method receive a front-end payment and create a chage
+ *       description: You can send a cart informations and payment token (https://stripe.com/docs/api/tokens).
+ *       tags: [stripe]
+ *       produces:
+ *         - application/json
+ *       security:
+ *         - ApiKeyAuth: []
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/x-www-form-urlencoded:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 stripeToken:
+ *                   description: "The API token, you can use this example to get it: https://stripe.com/docs/stripe-js/elements/quickstart"
+ *                   type: string
+ *                 order_id:
+ *                   description: The order ID recorded before (Check the Order Documentation)
+ *                   type: integer
+ *                 description:
+ *                   description: Description of the order
+ *                   type: string
+ *                 amount:
+ *                   description: "Amount in cents, only numbers like: 999"
+ *                   type: integer
+ *                 currency:
+ *                   description: "Check here the options: https://stripe.com/docs/currencies, the default"
+ *                   type: string
+ *               required:
+ *                 - stripeToken
+ *                 - order_id
+ *                 - description
+ *                 - amount
+ *       responses:
+ *         200:
+ *           description:  Object from Stripe
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 properties:
+ *                   orderId:
+ *                     type: integer
+ *                     example: 1
+ *         400:
+ *           description: Returns an error object
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Error'
+ *           examples:
+ *             User Key empty:
+ *               status: 401
+ *               code: AUT_01
+ *               message: Authorization code is empty.
+ *             User Key invalid:
+ *               status: 401
+ *               code: AUT_02
+ *               message: Access Unauthorized.
+ *             Empty stripe token:
+ *               code: USR_02
+ *               message: Stripe token can't be blank
+ *               field: stripeToken
+ *               status: 400
+ *             Empty order id:
+ *               code: USR_02
+ *               message: Order id can't be blank
+ *               field: order_id
+ *               status: 400
+ *             Invalid order id:
+ *               code: USR_03
+ *               message: Order id is not a number
+ *               field: order_id
+ *               status: 400
+ *             Empty description:
+ *               code: USR_02
+ *               message: Description can't be blank
+ *               field: description
+ *               status: 400
+ *             Empty amount:
+ *               code: USR_02
+ *               message: Amount can't be blank
+ *               field: amount
+ *               status: 400
+ *             Invalid amount:
+ *               code: USR_03
+ *               message: Amount is not a number
+ *               field: amount
+ *               status: 400
+ */
 router.post('/charge', [upload.none(), authMiddleware.verifyToken], stripeController.createCharge);
 
-/* Endpoint that provide a synchronization */
+
+/**
+ * Endpoint that provide a synchronization
+ *
+ *
+ * @swagger
+ * paths:
+ *   /stripe/webhooks:
+ *     post:
+ *       summary: Endpoint that provide a synchronization
+ *       description: "You need put this endpoint in the stripe webhooks (https://dashboard.stripe.com/account/webhooks), so get there the end-point secrete key."
+ *       tags: [stripe]
+ *       produces:
+ *         - application/json
+ *       responses:
+ *         200:
+ *           description:  This endpoint is used by Stripe.
+ */
 router.post('/webhooks', stripeController.handleWebhooks);
 
 module.exports = router;
